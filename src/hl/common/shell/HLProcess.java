26799192aa1238db7b7885c0f638004e590c1cb3
onghuilam <onghuilam@gmail.com>
2018-11-08 12:28:37 +0800

    process wrapper

--- src/hl/common/shell/HLProcess.java
+++ src/hl/common/shell/HLProcess.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
 
 public class HLProcess implements Runnable
 {
-	private PrintStream out 			= System.out;
+	private PrintStream out 			= null;//System.out;
 	
 	private String id					= null;
 	private List<String> listOutput		= new ArrayList<String>();
