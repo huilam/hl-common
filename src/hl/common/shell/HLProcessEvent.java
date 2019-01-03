c9ec8735c25ca5891cbd10bf9c97e3b7ce7821c9
onghuilam <onghuilam@gmail.com>
2019-01-03 09:37:07 +0800

    splitting process into separate project

--- src/hl/common/shell/HLProcessEvent.java
+++
@@ -1,8 +0,0 @@
-package hl.common.shell;
-
-public interface HLProcessEvent 
-{
-	void onProcessStart(HLProcess p);
-	void onProcessError(HLProcess p, Throwable e);
-	void onProcessTerminate(HLProcess p);
-}
