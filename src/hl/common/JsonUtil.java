5e9da1713c098b2c5af3762dfd545c3c4eb9dee7
onghuilam <onghuilam@gmail.com>
2019-05-08 16:14:32 +0800

    support multiple output from one key

--- src/hl/common/JsonUtil.java
+++ src/hl/common/JsonUtil.java
@@ -18,6 +18,7 @@ public class JsonUtil {
 		if(aConvertMapping!=null)
 		{
 			Object objValue = null;
+			
 			for(String sKey : aConvertMapping.keySet())
 			{
 				//Navigate JSON value
@@ -27,47 +28,56 @@ public class JsonUtil {
 				
 				if(objValue!=null)
 				{
+					
 					//Construct new structure
-					String sNewAttrName = aConvertMapping.get(sKey);
+					String sNewAttrNames = aConvertMapping.get(sKey);
 										
-//System.out.println("sNewAttrName==>"+sNewAttrName);
-					Stack<String> stacks = new Stack<String>();
-					StringTokenizer tk = new StringTokenizer(sNewAttrName, ".");
-					while(tk.hasMoreTokens())
-					{ 
-						String sNewSubKey = tk.nextToken();
-//System.out.println(" - sNewSubKey="+sNewSubKey);
-						stacks.push(sNewSubKey);
-					}
-	
-					JSONObject jsonAttr = null;
-					while(!stacks.isEmpty())
+					StringTokenizer tkNewAttrNames = new StringTokenizer(sNewAttrNames, ",");
+					
+					String sNewAttrName = null;
+					while(tkNewAttrNames.hasMoreTokens())
 					{
-						sNewAttrName = stacks.pop();
+						sNewAttrName = tkNewAttrNames.nextToken();
 						
-						if(jsonAttr==null)
+	//System.out.println("sNewAttrName==>"+sNewAttrName);
+						Stack<String> stacks = new Stack<String>();
+						StringTokenizer tk = new StringTokenizer(sNewAttrName, ".");
+						while(tk.hasMoreTokens())
+						{ 
+							String sNewSubKey = tk.nextToken();
+	//System.out.println(" - sNewSubKey="+sNewSubKey);
+							stacks.push(sNewSubKey);
+						}
+		
+						JSONObject jsonAttr = null;
+						while(!stacks.isEmpty())
 						{
-							jsonAttr = jsonOutput.optJSONObject(sNewAttrName);
+							sNewAttrName = stacks.pop();
+							
 							if(jsonAttr==null)
 							{
-								jsonAttr = new JSONObject();
+								jsonAttr = jsonOutput.optJSONObject(sNewAttrName);
+								if(jsonAttr==null)
+								{
+									jsonAttr = new JSONObject();
+								}
+								jsonAttr.put(sNewAttrName, objValue);
 							}
-							jsonAttr.put(sNewAttrName, objValue);
-						}
-						else
-						{
-							JSONObject jsonTemp = jsonAttr.optJSONObject(sNewAttrName);
-							if(jsonTemp==null)
+							else
 							{
-								jsonTemp = new JSONObject();
+								JSONObject jsonTemp = jsonAttr.optJSONObject(sNewAttrName);
+								if(jsonTemp==null)
+								{
+									jsonTemp = new JSONObject();
+								}
+								jsonTemp.put(sNewAttrName, jsonAttr);
+								jsonAttr = jsonTemp;
 							}
-							jsonTemp.put(sNewAttrName, jsonAttr);
-							jsonAttr = jsonTemp;
 						}
+						
+						//merge
+						jsonOutput = merge(jsonOutput, jsonAttr);
 					}
-					
-					//merge
-					jsonOutput = merge(jsonOutput, jsonAttr);
 				}
 			}
 		}
@@ -164,42 +174,20 @@ public class JsonUtil {
 	
 	public static void main(String args[])
 	{
-		String sData = "{\"timestamp\": 1557109653046," + 
-				"\"frameId\": 1," + 
-				"\"sensorId\": 1," + 
-				"\"feature\": \"VjAyMDAwMCAKa8cjIP…\"," + 
-				"\"picture\": \"/9j/4AAQSkZJRgA…\"," + 
-				"\"faceId\": -1," + 
-				"\"face_area_x\": 158," + 
-				"\"face_area_y\": 290," + 
-				"\"face_area_width\": 278," + 
-				"\"face_area_height\": 278," + 
-				"\"head_area_x\": 108," + 
-				"\"head_area_y\": 161," + 
-				"\"head_area_width\": 375," + 
-				"\"head_area_height\": 461," + 
-				"\"left_eye_x\": 369," + 
-				"\"left_eye_y\": 351," + 
-				"\"right_eye_x\": 219," + 
-				"\"right_eye_y\": 356," + 
-				"\"pan\": -4.8475122451782226562," + 
-				"\"roll\": 1.9091522693634033203," + 
-				"\"tilt\": 1.3301063776016235352," + 
-				"\"frontal_score\": 0.60400390625," + 
-				"\"quality\": 0.7776355743408203125," + 
-				"\"reliability\": 0.99921792745590209961," + 
-				"\"has_feature\": true," + 
-				"\"error\": \"\"" + 
-				"}";
+		String sData = "{\"priorityName\":\"HIGH\",\"markDuplicatedAs\":null,\"alertTypeId\":1,\"resourceId\":9,\"thumbnailType\":\"url\",\"subjectType\":\"PERSON\",\"subjectId\":\"17\",\"alertCategoryId\":4,\"priorityId\":1,\"videoUri\":null,\"alertStatusId\":1,\"originalImageUri\":\"/scc/data/DEFAULT/MATRIX_VMS/20190506/1223/CAMERA_08/1557116623418.jpg\",\"credibility\":0.83564126,\"alertTimestamp\":1557116623418,\"alertId\":7978,\"alertStatusName\":\"OPEN\",\"lastUpdatedBy\":null,\"alertLng\":\"103.80217552185059\",\"lastUpdatedTimestamp\":1557116624598,\"sourceSystemId\":1,\"videoType\":null,\"createdTimestamp\":1557116624598,\"vmsDeviceId\":\"38c3e712-1641-42fe-b0bf-395e480d5e70\",\"resourceName\":\"CAMERA_08\",\"alertLat\":\"1.2979064848506108\",\"videoEndTimestamp\":null,\"alertCategoryName\":\"TAG_N_TRACK.DEFAULT\",\"analyticEventId\":\"e4d35067-7577-4166-b7de-0ce0479cce48\",\"alertTypeName\":\"PERSON_DETECTION\",\"thumbnailData\":\"/scc/data/DEFAULT/MATRIX_VMS/20190506/1223/CAMERA_08/1557116623418_231_1330_184_212_THUMBNAIL.jpg\",\"createdBy\":null,\"videoStartTimestamp\":null,\"sourceSystemName\":\"MATRIX_VMS\",\"detail\":{\"processingResolution\":{\"top\":0,\"left\":0,\"width\":1920,\"height\":1080},\"originalResolution\":{\"width\":1920,\"height\":1080},\"poi\":{\"elected\":{\"personFaceId\":\"17\",\"personExtReference\":\"NEC_KUANYI\",\"score\":0.83564126,\"minThreshold\":0.6,\"personId\":\"17\",\"category\":\"DEFAULT\",\"repository\":\"TAG_N_TRACK\"},\"faceRegion\":{\"top\":300,\"left\":1371,\"width\":99,\"height\":100},\"headRegion\":{\"top\":231,\"left\":1330,\"width\":184,\"height\":212},\"idmPersonMeta\":{\"idmPersonId\":\"10eb55a3-5565-42dc-b5e5-28320f6dee7b\"},\"frontalFaceScore\":0.5859375,\"faceQualityScore\":0.84173197}},\"incidentId\":3,\"thumbnailRegion\":{\"top\":231,\"left\":1330,\"width\":184,\"height\":212}}";
+
+		sData = "{\"timestamp\":1551677822971,\"frameId\":1,\"sensorId\":1,\"feature\":\"VjAyMDAwM...\",\"picture\":\"/9j/4AAQSkZ...\",\"faceId\":-1,\"face_area_x\":175,\"face_area_y\":43,\"face_area_width\":38,\"face_area_height\":38,\"head_area_x\":168,\"head_area_y\":26,\"head_area_width\":52,\"head_area_height\":63,\"left_eye_x\":204,\"left_eye_y\":51,\"right_eye_x\":183,\"right_eye_y\":52,\"pan\":2.5689017772674560547,\"roll\":2.7263109683990478516,\"tilt\":-3.9310748577117919922,\"frontal_score\":0.60546875,\"quality\":0.88555473089218139648,\"reliability\":0.990497589111328125,\"has_feature\":true,\"error\":\"\"}";
+		
 		JSONObject jsonData = new JSONObject(sData);
 
+		
 		Map<String, String> mapRename = new HashMap<String, String>();
 		//mapRename.put("feature","feature");
 		mapRename.put("face_area_x","faceRegion.left");
 		mapRename.put("face_area_y","faceRegion.top");
 		mapRename.put("face_area_width","faceRegion.width");
 		mapRename.put("face_area_height","faceRegion.height");
-		
+		/**
 		mapRename.put("head_area_x","headRegion.left");
 		mapRename.put("head_area_y","headRegion.top");
 		mapRename.put("head_area_width","headRegion.width");
@@ -213,12 +201,17 @@ public class JsonUtil {
 		mapRename.put("tilt","faceTilt");
 		mapRename.put("frontal_score","frontalFaceScore");
 		mapRename.put("quality","faceQualityScore");
-		mapRename.put("reliability","faceScore");
+		**/
+		mapRename.put("reliability","xxx.yyy.faceScore,xxx.facefaceScore");
+
+		String sAttrName = "detail.processingResolution.width";
+		System.out.println(sAttrName+"="+JsonUtil.getJsonObj(jsonData, sAttrName));
+		
 		
 		JSONObject jsonConverted = JsonUtil.convert(jsonData, mapRename);
 		System.out.println("converted="+jsonConverted.toString());
 		
-		System.out.println("value="+JsonUtil.getJsonObj(jsonConverted, "headRegion.height"));
+		
 	}
     
 }
